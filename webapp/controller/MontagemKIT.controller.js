f78d5da603b501a9f36575bf0fdd807ca36c06ee
evandrolyrio <evandrofreitas94@gmail.com>
2023-03-07 10:58:44 -0300

    [Evandro Lyrio ] 07.03.2023

--- webapp/controller/MontagemKIT.controller.js
+++ webapp/controller/MontagemKIT.controller.js
@@ -123,7 +123,7 @@ sap.ui.define([
 							that.getModel("viewModel").setProperty("/busy", false);
 							that.getView().byId("tbMontaKIT").getBinding("items").refresh();
 						}
-						// that.lerCod();
+						that.lerCod();
 					},
 					error: function(error) {
 						that.getModel("viewModel").setProperty("/busy", false);
